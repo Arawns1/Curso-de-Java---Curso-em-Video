79157b8bf3239a63a3c88c61a5902676fd80a7d0
Gabriel <gabrieldamico22@gmail.com>
2022-08-31 21:45:21 -0300

    Até a aula 13

---
+++ AplicaçãoFor/src/aplicaçãofor/AplicaçãoFor.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aplicaçãofor;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class AplicaçãoFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for(int i = 0; i <= 3; i++ ){
+            for(int j = 0; j <= 2; j+=2){
+               System.out.println("i: " + i + " j: " + j);
+                
+            }
+                
+        }
+        
+    }
+  
+    
+  
+        
+  
+    }
+
